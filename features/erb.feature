686c2925ecfd567a8b2b18c834bea358d3ce75b7
Brian H. Ward <glyphrider@gmail.com>
2015-01-21 22:18:45 -0500

    new feature to test real addition

--- features/erb.feature
+++ features/erb.feature
@@ -3,3 +3,7 @@ Feature: Erlang Release Build
 Scenario: Call REST Method
 When I GET http://localhost:8080
 Then I receive {"sum":5}
+
+Scenario: POST
+When I POST {"addends":[4,5,6,7]} to http://localhost:8080
+Then I receive {"sum":22}
--- features/step_defs/erb_steps.rb
+++ features/step_defs/erb_steps.rb
@@ -5,6 +5,11 @@ When(/^I GET (.*)$/) do |url|
   @response = RestClient.get url
 end
 
+When(/^I POST (.*?) to (.*)$/) do |json,url|
+  @response = RestClient.post url,json
+end
+
+
 Then(/^I receive (.*)$/) do |body|
   expect(@response.code).to eq(200)
   expect(@response.body).to eq(body)
